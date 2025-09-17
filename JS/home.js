61c94cd9a323c9048e4db20e572a10e4f76b3d10
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2025-09-17 20:03:44 +0600

    Added cashout form and made Withdraw money button functional

--- JS/home.js
+++ JS/home.js
@@ -2,7 +2,6 @@
 document.getElementById("add-money-btn").addEventListener('click',function(e){
     e.preventDefault()
     const Bank=document.getElementById("bank").value
-    
     const accountNumber=document.getElementById("account-num").value
    // Account Number Validation
     if(accountNumber.length !==11){
@@ -24,6 +23,25 @@ document.getElementById("add-money-btn").addEventListener('click',function(e){
     const newBalance=currentBalance+addAmount;
     document.getElementById("current-balance").innerText=newBalance
        
+})
+
+// Withdraw Money Button
+document.getElementById("withdraw-btn").addEventListener('click',function(e){
+    e.preventDefault()
+    const agentNumber=document.getElementById("agent-num").value
+    const amount=parseInt(document.getElementById("withdraw-amount").value)
+    const pinNum=parseInt(document.getElementById("your-pin").value)
+    const currentMoney=parseInt(document.getElementById("current-balance").innerText)
+    const newAmount=currentMoney-amount;
+    console.log(agentNumber,amount,pinNum,currentMoney,newAmount)
+    //Amount Validation
+    if(currentMoney< amount)
+    {
+        alert("You do not have sufficient amount")
+        return;
+    }
+    document.getElementById("current-balance").innerText=newAmount 
+    
 })
 
 //toggling Feature
