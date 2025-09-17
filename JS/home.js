ebb6bea05aa4340c0f0cd20172ad60840783416b
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2025-09-17 16:53:01 +0600

    Added validation of Account number and Pin Number

--- JS/home.js
+++ JS/home.js
@@ -2,13 +2,27 @@
 document.getElementById("add-money-btn").addEventListener('click',function(e){
     e.preventDefault()
     const Bank=document.getElementById("bank").value
+    
     const accountNumber=document.getElementById("account-num").value
+   // Account Number Validation
+    if(accountNumber.length !==11){
+        alert("Invalid Account Number or Pin")
+        return;
+    }
+    
     const addAmount=parseInt(document.getElementById("add-amount").value)
-    const accountPin=document.getElementById("account-pin").value
+   
+    const accountPin=parseInt(document.getElementById("account-pin").value)
+   //Pin Number Validation
+    validPin=1234;
+    if(accountPin!==validPin){
+        alert("Invalid Account Number or Pin")
+        return;
+    }
+    
     const currentBalance=parseInt(document.getElementById("current-balance").innerText)
-    
-    
-    const newBalance=currentBalance+addAmount
+    const newBalance=currentBalance+addAmount;
     document.getElementById("current-balance").innerText=newBalance
     
+
 })
