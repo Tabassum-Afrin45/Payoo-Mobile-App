a0e9b06dbea214af9faf0827eeaa6b578a3e5ced
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2025-09-28 14:44:18 +0600

    Used Resuable fuction for grtting input values

--- JS/home.js
+++ JS/home.js
@@ -1,18 +1,29 @@
+//Function to get Input Values
+function getInputValue(id){
+    const inputValue=document.getElementById(id).value
+    return inputValue;
+}
+
+function getInputValueNumber(id){
+    const inputValue=document.getElementById(id).value
+    const inputValueNumber=parseInt(inputValue)
+    return inputValueNumber;
+}
+
 // Add Money Button Functionality
 document.getElementById("add-money-btn").addEventListener('click',function(e){
     e.preventDefault()
-    const Bank=document.getElementById("bank").value
-    const accountNumber=document.getElementById("account-num").value
-   // Account Number Validation
+    const Bank=getInputValue("bank")
+    const accountNumber=getInputValue("account-num")
+    const addAmount=getInputValueNumber("add-amount")
+    const accountPin=getInputValueNumber("account-pin")
+
+    // Account Number Validation
     if(accountNumber.length !==11){
         alert("Invalid Account Number or Pin")
         return;
     }
-    
-    const addAmount=parseInt(document.getElementById("add-amount").value)
-   
-    const accountPin=parseInt(document.getElementById("account-pin").value)
-   //Pin Number Validation
+    //Pin Number Validation
     validPin=1234;
     if(accountPin!==validPin){
         alert("Invalid Account Number or Pin")
@@ -25,21 +36,32 @@ document.getElementById("add-money-btn").addEventListener('click',function(e){
        
 })
 
-// Withdraw Money Button
+// Withdraw Money Button Functionality
 document.getElementById("withdraw-btn").addEventListener('click',function(e){
     e.preventDefault()
-    const agentNumber=document.getElementById("agent-num").value
-    const amount=parseInt(document.getElementById("withdraw-amount").value)
-    const pinNum=parseInt(document.getElementById("your-pin").value)
+    const agentNumber=getInputValue("agent-num")
+    const withdrawAmount=getInputValueNumber("withdraw-amount")
+    const pinNum=getInputValueNumber("your-pin")
+    
+    // Account Number Validation
+    if(agentNumber.length!==11){
+        alert("Invalid Agent Number or Pin")
+        return;
+    }
+    //Pin Number Validation
+    validPin=1234;
+    if(pinNum!==validPin){
+        alert("Invalid Agent Number or Pin")
+        return;
+    }
     const currentMoney=parseInt(document.getElementById("current-balance").innerText)
-    const newAmount=currentMoney-amount;
-    console.log(agentNumber,amount,pinNum,currentMoney,newAmount)
     //Amount Validation
-    if(currentMoney< amount)
+    if(currentMoney< withdrawAmount)
     {
         alert("You do not have sufficient amount")
         return;
     }
+    const newAmount=currentMoney-withdrawAmount;
     document.getElementById("current-balance").innerText=newAmount 
     
 })
